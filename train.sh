2af83c21c6fe8f344bbe9ba925c4c94a23c23fe2
Sourabh <sourabh19113@iiitd.ac.in>
2023-12-21 01:46:55 +0530

    corrected commands

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python  --data_dir $1 train.py --satellite $2 --task crop_type --run_name "${2}_${3}" --model $3 &&
-python  --data_dir $1 train.py --satellite $2 --task sowing_date --run_name "${2}_${3}" --model $3 &&
-python  --data_dir $1 train.py --satellite $2 --task transplanting_date --run_name "${2}_${3}" --model $3 &&
-python  --data_dir $1 train.py --satellite $2 --task harvesting_date --run_name "${2}_${3}" --model $3 &&
-python  --data_dir $1 train.py --satellite $2 --task crop_yield --run_name "${2}_${3}" --model $3 &&
-python  --data_dir $1 train.py --satellite $2 --task crop_yield --run_name "${2}_${3}_season" --model $3 --actual_season
+python train.py --data_dir $1 --satellite $2 --task crop_type --run_name "${2}_${3}" --model $3 &&
+python train.py --data_dir $1 --satellite $2 --task sowing_date --run_name "${2}_${3}" --model $3 &&
+python train.py --data_dir $1 --satellite $2 --task transplanting_date --run_name "${2}_${3}" --model $3 &&
+python train.py --data_dir $1 --satellite $2 --task harvesting_date --run_name "${2}_${3}" --model $3 &&
+python train.py --data_dir $1 --satellite $2 --task crop_yield --run_name "${2}_${3}" --model $3 &&
+python train.py --data_dir $1 --satellite $2 --task crop_yield --run_name "${2}_${3}_season" --model $3 --actual_season
